3e4e10f870f50f50baf0143b8cf7dba27b48b799
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-09-18 21:34:10 -0500

    - Removed unnecessary elements

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -1,16 +1,6 @@
 import {useState} from 'react';
 import './App.css';
 
-const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-]
-
 const Button = ({text, onClick}) => {
     return (
         <button onClick={onClick}>
@@ -64,19 +54,8 @@ const Statistics = ({feedback}) => {
     );
 };
 
-const Anecdote = ({quote}) => {
-    return (
-        <h3>
-            {quote}
-        </h3>
-    )
-}
-
-
 function App() {
     const [feedback, setFeedback] = useState({good: 0, neutral: 0, bad: 0});
-    const [quote, setQuote] = useState('')
-    const [votes, setVotes] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0})
 
     const increaseGood = () => {
         const newValue = feedback.good + 1;
@@ -95,35 +74,6 @@ function App() {
         setFeedback({...feedback, bad: newValue});
     };
 
-    const newQuote = () => {
-        const randomIndex = Math.round(Math.random() * anecdotes.length)
-
-        setQuote(anecdotes[randomIndex])
-    }
-
-    const increaseVote = () => {
-        const quoteIndex = Number(anecdotes.indexOf(quote))
-
-        const copy = {...votes} // Editing objects in this manner is much simpler, just make a copy of the object and alter it
-        copy[quoteIndex] = copy[quoteIndex] + 1
-        setVotes(copy)
-    }
-
-    const topAnecdote = () => {
-        let highestValue = 0;
-        let highestKey = 0;
-
-
-        for (const [key, value] of Object.entries(votes)) {
-            if (value > highestValue) {
-                highestValue = value;
-                highestKey = key;
-            }
-        }
-
-        return [highestKey, highestValue]
-    }
-
     return (
         <div className="App">
             <section className={'buttons'}>
@@ -139,22 +89,6 @@ function App() {
                     <h1>Statistics</h1>
                 </header>
                 <Statistics feedback={feedback}/>
-            </section>
-            <section>
-                <header>
-                    <h1>Generate Anecdote</h1>
-                </header>
-                <Anecdote quote={quote} votes={votes}/>
-                <p>Votes: {votes[anecdotes.indexOf(quote)]}</p>
-                <Button text={'Generate'} onClick={newQuote}/>
-                <Button text={'Vote!'} onClick={increaseVote}/>
-            </section>
-            <section className={'top-anecdote'}>
-                <header>
-                    <h1>Top Anecdote</h1>
-                </header>
-                <h3>{anecdotes[topAnecdote()[0]]}</h3>
-                <p>With {topAnecdote()[1]} votes!</p>
             </section>
         </div>
     );
